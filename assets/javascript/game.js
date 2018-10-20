8f9104554e747bbf100a9553f60a50051c6e52dc
Chieftjz <taylor.zlomke@gmail.com>
2018-10-20 17:41:33 -0600

    grabbed DOM elements,created newGame function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,11 @@
+//Grab DOM Elements
+var currentWord = document.getElementById("current-word");
+var guesses = document.getElementById("guesses");
+var lettersGuessed = document.getElementById("letters-guessed");
+var winCount = document.getElementById("win-count");
+var lossCount = document.getElementById("loss-count");
+
+
 //Global Variables
 var wordBank = ["lcd soundsystem", "the strokes", "yeah yeah yeahs", "tv on the radio", "the national", "interpol", "vampire weekend",];
 var wins = 0;
@@ -8,3 +16,50 @@ var gameRunning = false;
 var pickedWordPlaceholder = [];
 var guessedLetters = [];
 var incorrectLetters = [];
+
+
+function newGame() {
+    gameRunning = true;
+    guessesRemaining = 10;
+    guessedLetters = [];
+    pickedWordPlaceholder: [];
+    incorrectLetters = [];
+    
+    //pick a word
+    pickedWord = wordBank[Math.floor(Math.random)() * wordBank.length]
+}
+
+
+//Letter Guess Function
+function letterGuess() {
+
+}
+
+//Incorrect Checker
+function incorrectGuessCheck() {
+
+}
+
+//Loss Checker
+function lossChecker() {
+
+}
+
+//Win Checker
+function winChecker() {
+
+}
+
+//New Game Event Listner
+
+
+//onkeyup for letter guesses
+
+//Placeholder Creation
+for (var i = 0; i < pickedWord.length; i++) {
+    if (pickedWord[i] === " ") {
+        pickedWordPlaceholder.push(" ");
+    } else {
+        pickedWordPlaceholder.push("_");
+    }
+}
