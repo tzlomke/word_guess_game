665587444b1487f4e3540fbfcce64f85b7c1cdf8
Chieftjz <taylor.zlomke@gmail.com>
2018-10-19 23:01:26 -0600

    created basic html and js variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//Global Variables
+var wordBank = ["lcd soundsystem", "the strokes", "yeah yeah yeahs", "tv on the radio", "the national", "interpol", "vampire weekend",];
+var wins = 0;
+var losses = 0;
+var pickedWord = "";
+var guessesRemaining = 10;
+var gameRunning = false;
+var pickedWordPlaceholder = [];
+var guessedLetters = [];
+var incorrectLetters = [];
