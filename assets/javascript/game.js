46dd0f5df2423757a6c9e4fdf05b524a6bcce823
Chieftjz <taylor.zlomke@gmail.com>
2018-10-21 20:59:25 -0600

    completed stylesheet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,8 @@ var lettersGuessed = document.getElementById("letters-guessed");
 var winCount = document.getElementById("win-count");
 var lossCount = document.getElementById("loss-count");
 
-
 //Global Variables
-var wordBank = ["Pam", "Jim", "Michael", "Dwight", "Angela", "Kevin", "Oscar", "Kelly", "Ryan", "Darryl", "Stanley", "Toby", "Meredith", "Creed", "Phyllis", "Andy", "Erin"];
+var wordBank = ["Pam", "Jim", "Michael", "Dwight", "Angela", "Kevin", "Oscar", "Kelly", "Ryan", "Darryl", "Stanley", "Toby", "Meredith", "Creed", "Phyllis", "Andy", "Erin", "Gabe", "Holly"];
 var wins = 0;
 var losses = 0;
 var pickedWord = "";
@@ -16,6 +15,10 @@ var gameRunning = false;
 var pickedWordPlaceholder = [];
 var previouslyGuessed = [];
 var incorrectLetters = [];
+
+//Sounds
+wrongAudio = new Audio('/word_guess_game/assets/sounds/wrong.mp3');
+rightAudio = new Audio('right.mp3');
 
 //New Game Function
 function newGame() {
@@ -45,7 +48,6 @@ function newGame() {
     lossCount.textContent = losses;
 }
 
-
 //Game Logic
 function choice(letter) {
 
@@ -57,54 +59,58 @@ function choice(letter) {
                 pickedWordPlaceholder[i] = pickedWord[i];
             }
         }
-        
         currentWord.textContent = pickedWordPlaceholder.join(" ");
     } else if (gameRunning === true) {
         alert("You've already used this letter, you ignorant slut.");
     }
-    
+
     // Incorrect Guess Checker
     if (pickedWordPlaceholder.indexOf(letter.toLowerCase()) === -1 && pickedWordPlaceholder.indexOf(letter.toUpperCase())) {
         if (incorrectLetters.indexOf(letter) === -1) {
             incorrectLetters.push(letter);
-            guessesRemaining --;
-        }   
+            guessesRemaining--;
+        }
         lettersGuessed.textContent = incorrectLetters.join(" ");
         guesses.textContent = guessesRemaining;
     }
-    
+
     //Loss Checker
     if (guessesRemaining === 0) {
-        if (confirm("Wrong! Here...have the Extreme Repulsiveness Award. Would you like to try again?")) {
-            newGame();
-            losses ++;
-            lossCount.textContent = losses;
-            lettersGuessed.textContent = "";
-        }   
+        setTimeout (function () {
+            if (confirm("Wrong! Here...have the Extreme Repulsiveness Award. Would you like to try again?")) {
+                newGame();
+                losses++;
+                lossCount.textContent = losses;
+                lettersGuessed.textContent = "";
+                wrongAudio.play()
+            }
+        }, 10);
     }
-    
+
     //Win Checker
     if (pickedWordPlaceholder.join("") === pickedWord) {
-        if (confirm("You've won the Grace Under Fire Award! Want to go again?")) {
-            newGame();
-            wins ++;
-            winCount.textContent = wins;
-            lettersGuessed.textContent = "";
-        }
+        setTimeout(function () {
+            if (confirm("You've won the Grace Under Fire Award! Want to go again?")) {
+                newGame();
+                wins++;
+                winCount.textContent = wins;
+                lettersGuessed.textContent = "";
+            }
+        }, 10);
     }
 }
 
 //Start Game
 document.onkeypress = function () {
     if (gameRunning === false) {
+        alert("It's time for the Dundie Awards. For an event of this magnitude, name tags are imperative.")
         newGame();
     }
 }
 
 //onkeyup for letter guesses
-document.onkeydown = function(event) {
+document.onkeydown = function (event) {
     if (event.keyCode >= 65 && event.keyCode <= 90 && gameRunning === true) {
         choice(event.key);
     }
 }
-
